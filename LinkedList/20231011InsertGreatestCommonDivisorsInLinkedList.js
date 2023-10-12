ede7f3508cddfcbc2e9f494adb3d3afb35f2daff
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-10-11 19:16:12 -0600

    1011 Insert Greatest Common DivisorsIn Linked List

---
+++ LinkedList/20231011InsertGreatestCommonDivisorsInLinkedList.js
@@ -0,0 +1,19 @@
+// Given the head of a linked list head, in which each node contains an integer value.
+// Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+// Return the linked list after insertion.
+// The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertGreatestCommonDivisors = function(head) {
+    
+};
