8fa53fdd24e78bd9e23ad2414b52759aa2339d92
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-10-17 19:12:11 -0600

    Solve Jewels and Stones

--- HashTable/easy/JewelsAndStones.js
+++ HashTable/easy/JewelsAndStones.js
@@ -8,6 +8,25 @@
  * @param {string} stones
  * @return {number}
  */
-var numJewelsInStones = function(jewels, stones) {
-    
+var numJewelsInStones = function (jewels, stones) {
+  // Create a set to store the types of stones that are jewels
+  const jewelSet = new Set(jewels);
+  // Create a counter to count the number of stones that are also jewels
+  let counter = 0;
+
+  // Iterate through the string of stones using a for...of loop
+  for (const stone of stones) {
+    // If a stone is in the jewel set, increase the counter
+    if (jewelSet.has(stone)) {
+      counter++;
+    }
+  }
+
+  // Return the counter
+  return counter;
 };
+
+// The time complexity of this code is O(n), where n is the sum of the lengths of the input strings `jewels` and `stones`.
+// This is because the code iterates through both strings once.
+// The space complexity is O(n), where n is the length of the input string `jewels`.
+// This is because a set is created to store the `jewels`, which requires space proportional to the number of `jewels`.
