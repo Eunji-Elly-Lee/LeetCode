2bb8a9e831c26482909c9bdf092168515579cdb5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-10-28 21:30:01 -0600

    Solve Count Number of Pairs Absolute Difference K

--- CountNumberOfPairsWithAbsoluteDifferenceK.js
+++ CountNumberOfPairsWithAbsoluteDifferenceK.js
@@ -8,6 +8,32 @@
  * @param {number} k
  * @return {number}
  */
-var countKDifference = function(nums, k) {
-    
+var countKDifference = function (nums, k) {
+  // Create an object to store the frequency of each number
+  const numberFrequency = {};
+  // Create a counter to count the number of valid pairs
+  let counter = 0;
+
+  // Iterate through the array using a for...of loop
+  for (const number of nums) {
+    // If a number is in the frequency object, increase its frequency,
+    // otherwise, add the number with a value of 1
+    if (numberFrequency[number]) {
+      numberFrequency[number]++;
+    } else {
+      numberFrequency[number] = 1;
+    }
+
+    // If a number that is different by k is in the frequency object, add the frequency value to the counter
+    if (number - k in numberFrequency) {
+      counter += numberFrequency[number - k];
+    }
+
+    if (number + k in numberFrequency) {
+      counter += numberFrequency[number + k];
+    }
+  }
+
+  // Return the counter
+  return counter;
 };
