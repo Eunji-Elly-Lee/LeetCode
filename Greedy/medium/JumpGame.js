571761d648f94b4c51ba17b8fac5fbfd7f8a9d43
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-11-26 22:57:56 -0700

    Create Jump Game

---
+++ Greedy/medium/JumpGame.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/jump-game/
+// Array / Dynamic Programming / Greedy
+
+// You are given an integer array nums.
+// You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+// Return true if you can reach the last index, or false otherwise.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    
+};
