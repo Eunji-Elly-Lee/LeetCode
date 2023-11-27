40d68653bf7418b33f1cc6fda8b4cbdd84bb29c6
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-11-27 14:51:19 -0700

    Solve Best Time to Buy and Sell Stock II

--- Greedy/medium/BestTimeToBuyAndSellStockII.js
+++ Greedy/medium/BestTimeToBuyAndSellStockII.js
@@ -10,6 +10,26 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    
+var maxProfit = function (prices) {
+  // Create and initialize the maximum profit to 0
+  let maximumProfit = 0;
+
+  // Iterate through the input array using a for loop
+  for (let index = 1; index < prices.length; index++) {
+    // Calculate the price difference between the prices of the current day and the previous day
+    const priceDifference = prices[index] - prices[index - 1];
+
+    // If the price difference is positive, add it to the maximun profit
+    if (priceDifference > 0) {
+      maximumProfit += priceDifference;
+    }
+  }
+
+  // Return the maximum profit
+  return maximumProfit;
 };
+
+// The time complexity of this code is O(n), where n is the length of the `prices` array.
+// This is because the code iterates through the prices array once in the for loop.
+// The space complexity of this code is O(1), as it only uses a constant amount of additional space to store the `maximumProfit` variable.
+// The space used does not depend on the size of the input.
