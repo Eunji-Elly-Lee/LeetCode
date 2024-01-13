1c0a7dfc968f8588512322e0df41fa898c8168d7
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-01-12 22:12:25 -0700

    Add code for Partition Array According to Pivot (Code using Two Pointers)

--- Partitioning/medium/PartitionArrayAccordingToGivenPivot.js
+++ Partitioning/medium/PartitionArrayAccordingToGivenPivot.js
@@ -45,3 +45,43 @@ var pivotArray = function (nums, pivot) {
 // to store the elements that are less than, greater than, and equal to the pivot value.
 // The total space used by these arrays is proportional to the length of the input array.
 // Additionally, the code creates a new array `nums` to store the sorted elements, which also has a space complexity of O(n).
+
+// Using Two Pointers
+// Create and initialize a new array to store the rearranged elements and each pointer
+// const rearrangedArray = new Array(nums.length);
+// let leftPointer = 0;
+// let rightPointer = nums.length - 1;
+
+// // Iterate through the input array using a for loop from each endpoint
+// for (
+//   let leftIndex = 0, rightIndex = nums.length - 1;
+//   leftIndex < nums.length;
+//   leftIndex++, rightIndex--
+// ) {
+//   // Update the new array from each endpoint by comparing elements at each endpoint with a pivot
+//   if (nums[leftIndex] < pivot) {
+//     rearrangedArray[leftPointer] = nums[leftIndex];
+//     leftPointer++;
+//   }
+
+//   if (nums[rightIndex] > pivot) {
+//     rearrangedArray[rightPointer] = nums[rightIndex];
+//     rightPointer--;
+//   }
+// }
+
+// // Iterate through the input array using a for loop to fill in the middle of the array with numbers such as pivoting
+// for (let index = leftPointer; index <= rightPointer; index++) {
+//   rearrangedArray[index] = pivot;
+// }
+
+// // Return nums after rearranging with the new array
+// return (nums = [...rearrangedArray]);
+
+// The time complexity of this code is O(n), where n is the length of the input array `nums`.
+// This is because we iterate through the array once in the first for loop,
+// and then iterate through a portion of the array in the second for loop.
+// Both loops have a linear time complexity.
+// The space complexity of this code is O(n), where n is the length of the input array `nums`.
+// This is because we create a new array `rearrangedArray` with the same length as `nums`.
+// Therefore, the space required is proportional to the size of the input array.
