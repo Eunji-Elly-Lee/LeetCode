45ee7fadcce83e528a1866503994fb4df3d714f2
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-01-28 23:44:03 -0700

    Create Count Number of Nice Subarrays

---
+++ SlidingWindow/medium/CountNumberOfNiceSubarrays.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/count-number-of-nice-subarrays/description/
+// Array / Hash Table / Math/ Sliding Window
+
+// Given an array of integers nums and an integer k.
+// A continuous subarray is called nice if there are k odd numbers on it.
+// Return the number of nice sub-arrays.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubarrays = function (nums, k) {
+
+};
+
