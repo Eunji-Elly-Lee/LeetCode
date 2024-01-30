0a91d774fc87498382ba072d96371508f25dc395
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-01-29 22:25:43 -0700

    Create Count Complete Subarrays in an Array

---
+++ SlidingWindow/medium/CountCompleteSubarraysInAnArray.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/count-complete-subarrays-in-an-array/description/
+// Array / Hash Table / Sliding Window
+
+// You are given an array nums consisting of positive integers.
+// We call a subarray of an array complete if the following condition is satisfied:
+// The number of distinct elements in the subarray is equal to the number of distinct elements in the whole array.
+// Return the number of complete subarrays.
+// A subarray is a contiguous non-empty part of an array.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countCompleteSubarrays = function(nums) {
+    
+};
