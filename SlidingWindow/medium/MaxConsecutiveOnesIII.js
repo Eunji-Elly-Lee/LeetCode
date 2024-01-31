22b6dd99a9665371df362e3a3ee61dba83cdc11b
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-01-30 18:43:17 -0700

    Create Max Consecutive Ones III

---
+++ SlidingWindow/medium/MaxConsecutiveOnesIII.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/max-consecutive-ones-iii/
+// Array / Binary Search / Sliding Window / Prefix Sum
+
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    
+};
