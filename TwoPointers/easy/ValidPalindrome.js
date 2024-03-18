fd25a585c81f058f4ce12264dceb54806014d568
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-03-17 23:44:54 -0600

    Create Valid Palindrome

---
+++ TwoPointers/easy/ValidPalindrome.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/valid-palindrome/description/
+// Two Pointers / String
+
+// A phrase is a palindrome if,
+// after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+
+};
