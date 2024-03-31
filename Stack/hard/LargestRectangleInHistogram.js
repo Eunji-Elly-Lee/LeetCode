34947e174d6129ec42a1829747690ea4e0728cea
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-03-30 23:16:42 -0600

    Create Largest Rectangle in Histogram

---
+++ Stack/hard/LargestRectangleInHistogram.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+// Array / Stack / Monotonic Stack
+
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
+// return the area of the largest rectangle in the histogram.
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    
+};
