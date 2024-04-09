dfe7b9a4eeec403a2a6086493f1a100d47010b09
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-04-08 23:30:12 -0600

    Create Binary Search

---
+++ BinarySearch/easy/BinarySearch.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/binary-search/description/
+// Array / Binary Search
+
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+// If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {};
