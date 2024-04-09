c6d80265ee5454ddfa20eaedc3518770cb001c86
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-04-09 15:00:20 -0600

    Solve Binary Search

--- BinarySearch/easy/BinarySearch.js
+++ BinarySearch/easy/BinarySearch.js
@@ -10,4 +10,30 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {};
+var search = function (nums, target) {
+  // Create two variables to store and track left and right pointers
+  let left = 0;
+  let right = nums.length - 1;
+
+  // Iterate through the input array using a while loop
+  while (left <= right) {
+    // Calculate the middle index between the left and right pointers
+    let middle = Math.floor((left + right) / 2);
+
+    // Update pointers by comparing the number at the middle index
+    // If the target has been found, return the middle index
+    if (nums[middle] < target) {
+      left = middle + 1;
+    } else if (nums[middle] > target) {
+      right = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+
+  // Return -1 because the target hasn't been found
+  return -1;
+};
+
+// The time complexity of this binary search algorithm is O(log n) because the search space is halved in each iteration.
+// The space complexity is O(1) because only a constant amount of extra space is used regardless of the input size.
