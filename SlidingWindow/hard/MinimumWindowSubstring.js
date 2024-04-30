96c6249f33af3ad330b6183163b607677df61ecb
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-04-29 23:52:38 -0600

    Create Minimum Window Substring

---
+++ SlidingWindow/hard/MinimumWindowSubstring.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-window-substring/description/
+// Hash Table / String / Sliding Window
+
+// Given two strings s and t of lengths m and n respectively,
+// return the minimum window substring of s such that every character in t (including duplicates) is included in the window.
+// If there is no such substring, return the empty string "".
+// The testcases will be generated such that the answer is unique.
+// Follow up: Could you find an algorithm that runs in O(m + n) time?
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    
+};
