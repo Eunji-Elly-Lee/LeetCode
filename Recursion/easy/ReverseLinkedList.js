3bfb5b90f25172e02ce8f92235311e404048fc0f
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-05-04 22:56:08 -0600

    Create Reverse Linked List

---
+++ Recursion/easy/ReverseLinkedList.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/reverse-linked-list/description/
+// Linked List / Recursion
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// Follow up: A linked list can be reversed either iteratively or recursively.
+// Could you implement both?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+};
+
