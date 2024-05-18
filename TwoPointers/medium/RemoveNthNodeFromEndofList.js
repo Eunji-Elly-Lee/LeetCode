f068834d8a672e348dfa21b79a2f19a7f0f12e73
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-05-17 23:55:30 -0600

    Create Remove Nth Node From End of List

---
+++ TwoPointers/medium/RemoveNthNodeFromEndofList.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+// Linked List / Two Pointers
+
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+// Follow up: Could you do this in one pass?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+};
