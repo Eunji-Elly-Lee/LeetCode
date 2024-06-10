244a839f0171b57854e4e7b6abe98c3d2c98094b
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-06-09 23:55:30 -0600

    Create Reverse Nodes in k-Group

---
+++ Recursion/hard/ReverseNodesInKGroup.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+// Linked List / Recursion
+
+// Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+// k is a positive integer and is less than or equal to the length of the linked list.
+// If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+// You may not alter the values in the list's nodes, only nodes themselves may be changed.
+// Follow-up: Can you solve the problem in O(1) extra memory space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    
+};
