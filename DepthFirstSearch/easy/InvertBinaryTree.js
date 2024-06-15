6349e1b81a9bffb11a7d18ce1fd7dec5f4091f4e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-06-14 22:40:02 -0600

    Create Invert Binary Tree

---
+++ DepthFirstSearch/easy/InvertBinaryTree.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/invert-binary-tree/description/
+// Tree / Depth-First Search / Breadth-First Search / Binary Tree
+
+// Given the root of a binary tree, invert the tree, and return its root.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    
+};
