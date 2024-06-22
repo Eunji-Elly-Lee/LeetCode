ffe0a90dbba6e051e1e0e11cf081d4b155163392
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-06-21 23:55:30 -0600

    Create Balanced Binary Tree

---
+++ DepthFirstSearch/easy/BalancedBinaryTree.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/balanced-binary-tree/description/
+// Tree / Depth-First Search / Binary Tree
+
+// Given a binary tree, determine if it is height-balanced.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    
+};
